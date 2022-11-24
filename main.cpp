cc0b58ad0e99f8d3009d688a94e898b30fd9208f
MatthewMong <matthew.mong1999@gmail.com>
2022-11-24 13:50:35 -0800

    Update main.cpp
    ahhhhh
    
    Signed-off-by: MatthewMong <matthew.mong1999@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,26 +1,81 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <mbed.h>
+#include <rtos.h>
+#include <platform/Callback.h>
+#include <Arduino_LSM9DS1.h>
 
-#include "mbed.h"
+using namespace rtos;
+
+Semaphore s1(1);
+Semaphore s2(0);
+float gx, gy, gz, ax, ay, az;
+
+Thread t2;
+Thread t3;
+Thread t4;
 
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
-
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
-
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+void checkGyro(void) {
+  while (true) {
+    s1.acquire();
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(gx, gy, gz);
     }
+    s2.release();
+  }
 }
+
+void checkAccel(void) {
+  while (true) {
+    s1.acquire();
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(ax, ay, az);
+    }
+    s2.release();
+  }
+}
+
+void printData(void) {
+  while (true) {
+    s2.acquire();
+    Serial.println("Accel data: ");
+    Serial.print(ax);
+    Serial.print(",");
+    Serial.print(ay);
+    Serial.print(",");
+    Serial.println(az);
+
+    Serial.println("gyro data: ");
+    Serial.print(gx);
+    Serial.print(",");
+    Serial.print(gy);
+    Serial.print(",");
+    Serial.println(gz);
+
+    Serial.println((String)"Time is: " + us_ticker_read());
+    wait_us(100000);
+    s1.release();
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println("Hz");
+  t2.start(mbed::callback(checkGyro));
+  t3.start(mbed::callback(checkAccel));
+  t4.start(mbed::callback(printData));
+}
+
+void loop() {}
