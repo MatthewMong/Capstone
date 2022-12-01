78ead87340e031ecfd98bfaaf5049767ce2a3f0b
ayushbansal <}_EC:RPagv2@#_mz^+>
2022-12-01 11:10:16 -0800

    Updated Serial Read

--- serialRead.py
+++ serialRead.py
@@ -1,15 +1,16 @@
 import serial
+import datetime
+ser = serial.Serial('COM3', timeout=None, baudrate=9800)
 
-ser = serial.Serial('COM5', timeout=None, baudrate=9800) 
 import csv
-csvfile = open('test.csv', 'w', newline='')
+import time
+csvfile = open('test1.csv', 'w', newline='')
 spamwriter = csv.writer(csvfile)
 try:
     while True:
         data_raw = ser.readline()
         formatted = (data_raw).decode('ascii').split(",")
-
-        print(formatted[:-1])
-        spamwriter.writerow(formatted[: -1])
+        print(formatted)
+        spamwriter.writerow([*formatted[:-1], datetime.datetime.now().isoformat()])
 except KeyboardInterrupt:
     pass
