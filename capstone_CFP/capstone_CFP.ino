665df305d81529d9d126d50d0cc557ddafef0888
Matthew Mong <matthew.mong1999@gmail.com>
2023-01-02 00:52:19 -0800

    in progress: implement interupts

--- capstone_CFP/capstone_CFP.ino
+++ capstone_CFP/capstone_CFP.ino
@@ -6,34 +6,38 @@
 #include <Arduino_LSM9DS1.h>
 #include <Adafruit_LPS35HW.h>
 #include <SPI.h>
+#include <PluggableUSBMSD.h>
 
-const bool useBLE = false;
 using namespace rtos;
 using namespace events;
 
+// Flags
+const bool useBLE = false;
+
+// Constants
 const int RX_BUFFER_SIZE = 256;
 bool RX_BUFFER_FIXED_LENGTH = false;
 const char* nameOfPeripheral = "testDevice";
 const char* uuidOfService = "0000181a-0000-1000-8000-00805f9b34fb";
 const char* uuidOfTxChar = "00002a59-0000-1000-8000-00805f9b34fb";
+const PinName buttonPin = p14;  // the number of the pushbutton pin (Analog 0)
+volatile bool isLogging = true;      // volatile bool for logging flag
+float gx, gy, gz, ax, ay, az, pr;    // data values
+
+// BLE service
 BLEService sendService(uuidOfService);
 BLEFloatCharacteristic axChar(uuidOfTxChar, BLERead | BLENotify | BLEBroadcast);
 BLEFloatCharacteristic ayChar(uuidOfTxChar, BLERead | BLENotify | BLEBroadcast);
 BLEFloatCharacteristic azChar(uuidOfTxChar, BLERead | BLENotify | BLEBroadcast);
+BLEDevice peripheral;
 
 EventQueue queue(64 * EVENTS_EVENT_SIZE);
 
-BLEDevice peripheral;
-
-float gx, gy, gz, ax, ay, az, pr;
-
 Adafruit_LPS35HW lps35hw = Adafruit_LPS35HW();
 
-Thread t1;
-Thread t3;
-Thread t4;
-Thread t5;
+mbed::InterruptIn button(buttonPin);
 
+Thread t1;
 
 void checkGyro(void) {
   if (IMU.gyroscopeAvailable()) {
@@ -51,7 +55,12 @@ void checkBarom(void) {
   pr = lps35hw.readPressure();
 }
 
-void printData(void) {
+void startStopLogging(void) {
+  isLogging = !isLogging;
+  //TODO: need to close file and handle queue clearing
+}
+
+void printData(FILE* f) {
   if (useBLE) {
     BLEDevice central = BLE.central();
     if (central.connected()) {
@@ -60,7 +69,11 @@ void printData(void) {
       azChar.writeValue(az);
     }
   } else {
-    Serial.println((String)ax + "," + ay + "," + az + "," + gx + "," + gy + "," + gz + "," + pr + "," + us_ticker_read());
+    // Serial.println((String)ax + "," + ay + "," + az + "," + gx + "," + gy + "," + gz + "," + pr + "," + us_ticker_read());
+    if (isLogging) {
+      String value = (String)ax + "," + ay + "," + az + "," + gx + "," + gy + "," + gz + "," + pr + "," + us_ticker_read() + "\n";
+      fwrite(value.c_str(), value.length(), 1, f);
+    }
   }
 }
 
@@ -84,6 +97,7 @@ void setup() {
   Serial.print("Accelerometer sample rate = ");
   Serial.print(IMU.accelerationSampleRate());
   Serial.println("Hz");
+
   if (useBLE) {
     if (!BLE.begin()) {
       Serial.println("* Starting Bluetooth® Low Energy module failed!");
@@ -102,14 +116,16 @@ void setup() {
     // rxChar.setEventHandler(BLEWritten, onRxCharValueUpdate);
     BLE.advertise();
   }
-  queue.call_every(1, checkAccel);
-  queue.call_every(1, checkGyro);
-  queue.call_every(100, checkBarom);
-  queue.call_every(1, printData);
+
+  MassStorage.begin();
+  FILE* f = fopen("/fs/data.csv", "a+");
+  Serial.println("Init file system");
+  int accelID = queue.call_every(1, checkAccel);
+  int gyroID = queue.call_every(1, checkGyro);
+  int baromID = queue.call_every(100, checkBarom);
+  int printID = queue.call_every(1, printData, f);
+  button.rise(startStopLogging);
   t1.start(callback(&queue, &EventQueue::dispatch_forever));
-  // t3.start(mbed::callback(checkAccel));
-  // t4.start(mbed::callback(checkBarom));
-  // t5.start(mbed::callback(printData));
 }
 
 void loop() {}
